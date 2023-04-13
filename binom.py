1e5e0ce210139cdc25d37cba5b71ed8687acc356
saba <x3y2b@ugrad.cs.ubc.ca>
2023-04-13 03:19:07 -0700

    fixed negative binom

--- binom.py
+++ binom.py
@@ -1,27 +1,45 @@
-from scipy.stats import binom
-import matplotlib.pyplot as plt
-from read_epi import data
+import warnings
 
+from scipy.stats import nbinom
+import matplotlib.pyplot as plt, seaborn as sns
+import numpy as np
+import statsmodels.api as sm
+from sklearn.decomposition import PCA
+from read_epi import data
+import pandas as pd
+warnings.filterwarnings('ignore')
+pca = PCA(n_components=2)
+
+data = pd.read_csv("Brain_GSE50161.csv")
+data = data.to_numpy()
 row, column = data.shape
 # probability of having a cancer cell
 p = 0.2
-# defining the list of r values
-r_values = list(range(row + 1))
-# obtaining the mean and variance
-mean, var = binom.stats(row, p)
-# list of pmf values
-dist = [binom.pmf(r, row, p) for r in r_values ]
-# printing the table
+def get_hist_and_indices(X, i):
+    # this is just binning the original, continuous gene expression data into discrete bins (use these bins so that we are consistent!)
+    sample = X[:, i+2]
+    hist = np.histogram(sample, bins=15, density=False)
+    indices = np.digitize(np.float64(sample), np.float64(hist[1]), right=False) # don't think we need this, just in case tho
+    return sample, hist
 
-print("r\tp(r)")
-for i in range(row + 1):
-    print(str(r_values[i]) + "\t" + str(dist[i]))
-# printing mean and variance
-print("mean = "+str(mean))
-print("variance = "+str(var))
 
-plt.bar(r_values, dist)
+sample, hist = get_hist_and_indices(data, 4)
+
+print(sample.mean())
+print(sample.var())
+
+X = np.ones_like(sample)
+X = np.array(X, dtype=float)
+sample = np.array(sample, dtype=float)
+res = sm.NegativeBinomial(sample, X).fit(start_params=[1,1])
+print(res.summary)
+mu = np.exp(res.params[0])
+p = 1/(1+np.exp(res.params[0])*res.params[1])
+n = np.exp(res.params[0])*p/(1-p)
+x_plot = np.linspace(0,14,15)
+sns.set_theme()
+ax = sns.distplot(sample, kde=False, norm_hist=True, label='Real Values')
+ax.plot(x_plot, nbinom.pmf(x_plot, n, p), 'g-', lw=2, label = 'fitted NB')
+leg = ax.legend()
+plt.title('real vs. Fitted NB distribution')
 plt.show()
-
-print(data)
-print(row, column)
